c9cb780bacdc765f6d3f57b69027ac23ec5178bd
kiarashPvd <132096443+kiarashPvd@users.noreply.github.com>
2025-05-18 14:47:51 +0330

    fixing colors and display

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import pygame , time , random
+
+
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+
+width = 600
+height = 400
+
+dis = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game by Kiarash payervand')
